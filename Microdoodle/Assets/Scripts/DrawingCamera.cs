c51185f806c817e244d422ae43060103e000732a
LightChase <83929087+Yorcs@users.noreply.github.com>
2024-09-11 01:14:35 -0700

    Changed IDE, added drawing camera script

--- Microdoodle/Assets/Scripts/DrawingCamera.cs
+++ Microdoodle/Assets/Scripts/DrawingCamera.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DrawingCamera : MonoBehaviour
 {
+    public Camera drawingCamera;
+    public RawImage overlayImage;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        drawingCamera.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        overlayImage.texture = drawingCamera.targetTexture;
     }
 
     // Update is called once per frame
