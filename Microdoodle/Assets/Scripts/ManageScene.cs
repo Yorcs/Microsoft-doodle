7b7542ec161d6b9bc69268de1e899d190772a44b
LightChase <83929087+Yorcs@users.noreply.github.com>
2024-09-22 16:22:48 -0700

    sent variable from one scene to another
    - needs refactoring

--- Microdoodle/Assets/Scripts/ManageScene.cs
+++ Microdoodle/Assets/Scripts/ManageScene.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class ManageScene : MonoBehaviour
 {
+    private TMP_Text promptTextComponent;
+
     public void ChangeScene(int sceneID){
+        promptTextComponent = GetComponentInChildren<TMP_Text>();
+        string dataToKeep = promptTextComponent.text;
         SceneManager.LoadScene(sceneID);
     }
 }
