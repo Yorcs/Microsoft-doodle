e5d21c57ee5e4676a0e82ae4d517530c34e2bdb8
LightChase <83929087+Yorcs@users.noreply.github.com>
2024-09-22 17:18:46 -0700

    Change game name

--- SnapProm/Assets/Scripts/PromptTrigger.cs
+++ SnapProm/Assets/Scripts/PromptTrigger.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PromptTrigger : MonoBehaviour
 {
+    private TMP_Text promptTextComponent;
     public bool chosen = false;
+    private string dataToKeep;
 
     public void Selected(){
         if(chosen){
             Debug.Log("Selected");
+            promptTextComponent = GetComponentInChildren<TMP_Text>();
+            dataToKeep = promptTextComponent.text;
         }
     }
 }
