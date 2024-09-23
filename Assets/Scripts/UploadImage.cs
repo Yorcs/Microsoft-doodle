df2634a58117b797ea22d04e405c16b7f44bfcec
LightChase <83929087+Yorcs@users.noreply.github.com>
2024-09-22 18:13:54 -0700

    added image scene and upload image
    - needs to be tested
    - Players would be able to upload image

---
+++ Assets/Scripts/UploadImage.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.IO;
+using UnityEngine.Networking;
+
+public class UploadImage : MonoBehaviour
+{
+    string path;
+    public RawImage image;
+
+    private void Awake()
+    {
+        image = GetComponentInChildren<RawImage>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        OpenExplorer();
+    }
+
+    public void OpenExplorer()
+    {
+        path = EditorUtility.OpenFilePanel("Overwrite with png", "", "png");
+        GetImage();
+    }
+
+    void GetImage()
+    {
+        if (path != null)
+        {
+            UpdateImage();
+        }
+    }
+
+    void UpdateImage()
+    {
+        StartCoroutine(DownloadImage());
+    }
+
+    IEnumerator DownloadImage()
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture("file:///" + path);
+        yield return request.SendWebRequest();
+
+        if (request.isNetworkError || request.isHttpError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            image.texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+        }
+    }
+}
