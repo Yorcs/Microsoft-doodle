fce79ad73a4802c8d4b81dc8a4cec186730af323
LightChase <83929087+Yorcs@users.noreply.github.com>
2024-09-30 22:22:14 -0700

    added components for color picker, color textures

--- Assets/Scripts/ColorPickerScript.cs
+++ Assets/Scripts/ColorPickerScript.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
 
 public class ColorPickerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float currentHue;
+
+    [SerializeField] private RawImage hueImage, outputImage;
+
+    [SerializeField] private Slider hueSlider;
+
+    private Texture2D hueTexture, outputTexture;
+
+    [SerializeField] MeshRenderer changeThisColor;
+
+    private void CreateHueImage()
     {
-        
+        hueTexture = new Texture2D(1, 16);
+        hueTexture.wrapMode = TextureWrapMode.Clamp;
+        hueTexture.name = "HueTexture";
+
+        for (int i = 0; i < hueTexture.height; i++)
+        {
+            hueTexture.SetPixel(0, i, Color.HSVToRGB((float)i / hueTexture.height, 1, 0.05f));
+        }
+
+        hueTexture.Apply();
+        currentHue = 0;
+
+        hueImage.texture = hueTexture;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateOutputImage()
     {
-        
+        outputTexture = new Texture2D(1, 16);
+        outputTexture.wrapMode = TextureWrapMode.Clamp;
+        outputTexture.name = "OutputTexture";
+
+        Color currentColor = Color.HSVToRGB(currentHue, 255, 255);
+
+        for (int i = 0;i < outputTexture.height; i++)
+        {
+            outputTexture.SetPixel(0, i, currentColor);
+        }
+
+        outputTexture.Apply();
+        outputImage.texture = outputTexture;
     }
 }
